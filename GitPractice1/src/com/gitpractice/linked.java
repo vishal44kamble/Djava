e5eeb3530656b49ae315a23214d00f5f7c7d6f1a
vishal44kamble <vishal44kamble@gmail.com>
2022-09-13 17:18:03 +0530

    added code for linked list

---
+++ GitPractice1/src/com/gitpractice/linked.java
@@ -0,0 +1,16 @@
+package com.gitpractice;
+
+import java.util.LinkedList;
+
+public class linked {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> ll=new LinkedList<Integer>();
+		ll.add(10);
+		ll.add(20);
+		ll.add(30);
+		ll.add(40);
+		System.out.println(ll);
+	}
+
+}
