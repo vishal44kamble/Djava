0bef2fb349548f80d9c3b44f3834702f6c18b56d
vishal44kamble <vishal44kamble@gmail.com>
2022-09-13 17:08:29 +0530

    add arrayList code

---
+++ GitPractice1/src/com/gitpractice/Test1.java
@@ -0,0 +1,16 @@
+package com.gitpractice;
+
+import java.util.ArrayList;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		ArrayList<String> al=new ArrayList<String>();
+		al.add("vishal");
+		al.add("rahul");
+		al.add("anna");
+		al.add("mangesh");
+		System.out.println(al);
+	}
+
+}
